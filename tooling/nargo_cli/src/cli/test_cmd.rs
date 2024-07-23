8b96a17dbd0be0c22a3cde7ae0b32646506c3556
Ana Perez Ghiglia <aghiglia@manas.tech>
2024-07-23 17:47:51 -0300

    Ignore --debug mode for tests with arguments

--- tooling/nargo_cli/src/cli/test_cmd.rs
+++ tooling/nargo_cli/src/cli/test_cmd.rs
@@ -231,12 +231,13 @@ fn run_test<S: BlackBoxFunctionSolver<FieldElement> + Default>(
         use noir_fuzzer::FuzzedExecutor;
         use proptest::test_runner::TestRunner;
 
-        let compiled_program: Result<noirc_driver::CompiledProgram, noirc_driver::CompileError> =
-            if debug_mode {
-                compile_no_check_for_debug(&mut context, test_function, compile_options)
-            } else {
-                compile_no_check(&mut context, compile_options, test_function.get_id(), None, false)
-            };
+        if debug_mode {
+            println!(
+                "[{}] Ignoring --debug flag since debugging test with arguments is disabled",
+                package.name
+            );
+        }
+        let compiled_program: Result<noirc_driver::CompiledProgram, noirc_driver::CompileError> = compile_no_check(&mut context, compile_options, test_function.get_id(), None, false);
         match compiled_program {
             Ok(compiled_program) => {
                 let runner = TestRunner::default();
