0bba2ec605d42be0451cf639c0ae52a4c21838d6
Ana Perez Ghiglia <aghiglia@manas.tech>
2025-02-18 15:22:23 -0300

    set pedantic_solving when creating the BlackBoxSolver

--- tooling/nargo_cli/src/cli/debug_cmd.rs
+++ tooling/nargo_cli/src/cli/debug_cmd.rs
@@ -481,12 +481,12 @@ pub(crate) fn debug_program(
     package_name: String,
 ) -> Result<Option<WitnessStack<FieldElement>>, NargoError<FieldElement>> {
     noir_debugger::run_repl_session(
-        // &Bn254BlackBoxSolver(pedantic_solving),
         compiled_program,
         initial_witness,
         raw_source_printing,
         foreign_call_resolver_url,
         root_path,
         package_name,
+        pedantic_solving,
     )
 }
